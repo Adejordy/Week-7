9e7eda4d40d9efbefaf07f551e04f9109288b3fd
Alyssa <alyssadejordy@gmail.com>
2024-09-18 11:32:21 -0700

    add paragraph

--- script.js
+++ script.js
@@ -13,5 +13,14 @@ console.log(ages);
 //I used the push method to add age 16 to the array
 console.log((ages[0] -= ages.length - 1));
 //I'm not sure why is goes in (()), when I saved the first time it added them so I added them again to the second run
-for (let i = 0; i < ages.length; i++) i++;
-console.log(ages / [i]);
+//FIXME /* I'm not sure how to find the average age/ coming back to this part of the question:for (let i = 0; i < ages.length; i++) i++; console.log(ages / [i]);
+
+/*2.Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
+Use a loop to iterate through the array and calculate the average number of letters per name.
+Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
+*/
+let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+//FIXME calculate the average number of letters
+for (let i = 0; i < names.length; i++) {
+  console.log(names.join(" "));
+}
