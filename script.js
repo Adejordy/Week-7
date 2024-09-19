20bfa56a15c804903999413255ed44dc07b87ca8
Alyssa <alyssadejordy@gmail.com>
2024-09-19 08:33:39 -0700

    'add paragraph

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(ages);
 //I used the push method to add age 16 to the array
 console.log((ages[0] -= ages.length - 1));
 //I'm not sure why is goes in (()), when I saved the first time it added them so I added them again to the second run
-//FIXME /* I'm not sure how to find the average age/ coming back to this part of the question:for (let i = 0; i < ages.length; i++) i++; console.log(ages / [i]);
+//FIXME /* I'm not sure how to find the average age/ coming back to this part of the question:
 
 /*2.Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
 Use a loop to iterate through the array and calculate the average number of letters per name.
@@ -24,3 +24,32 @@ let names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 for (let i = 0; i < names.length; i++) {
   console.log(names.join(" "));
 }
+//3. How do you access the last element of any array?
+//Answer: by using arrayname.pop(),
+let arrayLength = ["First", "Middle", "Last"];
+{
+  console.log(arrayLength.pop());
+}
+//4. How do you access the first element of any array?
+//Answer: arrayname.shift
+arrayLength = ["First", "Middle", "Last"];
+{
+  console.log(arrayLength.shift());
+}
+//TODO 5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+
+//TODO 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.
+
+//TODO 7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in 'Hello' and 3, I would expect the function to return 'HelloHelloHello').
+
+//TODO 8. Write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by a space.
+
+//TODO 9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+
+//TODO 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
+
+//TODO 11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+
+//TODO 12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+
+//TODO 13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
